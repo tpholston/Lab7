2053e68468d8a3aec941c4edb8a27eea43c914e8
tpholston <tholston@ucsd.edu>
2021-05-16 21:53:08 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,19 +3,6 @@
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
 
-// Make sure you register your service worker here too
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
 var currId = 0;
 document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
@@ -44,4 +31,17 @@ document.querySelector("h1").addEventListener('click', () => {
 
 window.onpopstate = function(event) {
   setState(event.state, true);
+}
+
+// Make sure you register your service worker here too
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
 }
